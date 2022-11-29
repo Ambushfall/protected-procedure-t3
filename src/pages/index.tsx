e34bf4c67839afc032ad3622c70c4aac6c73bd25
Ambushfall <lukathetormentor@gmail.com>
2022-11-29 12:36:51 +0100

    Changed Sess data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,8 +69,9 @@ const AuthShowcase: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}<img alt="discord image" src={sessionData?.user?.image?.toString()} width={150} height={150} /></span>}
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
+        {sessionData && <img alt="discord image" src={sessionData?.user?.image?.toString()} width={150} height={150} />}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
